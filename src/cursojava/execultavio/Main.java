90b0db59441732538419fe18210e16adccec367a
Cesar Henrique Ramos <cesar252742@gmail.com>
2022-04-10 16:25:05 -0300

    Criado a classe

---
+++ src/cursojava/execultavio/Main.java
@@ -0,0 +1,13 @@
+package cursojava.execultavio;
+
+import cursojava.classes.Aluno;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Cesar");
+	}
+
+}
