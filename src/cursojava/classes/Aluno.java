ed3bae422290761dbc8606d4d51f55638c8e47fd
Cesar Henrique Ramos <cesar252742@gmail.com>
2022-04-11 18:17:48 -0300

    creado metodo e insetio dado pelo teclado

--- src/cursojava/classes/Aluno.java
+++ src/cursojava/classes/Aluno.java
@@ -10,6 +10,33 @@ public class Aluno {
 	String nomeMae;
 	String nomePai;
 	String dataMatricula;
+	
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	public double getNota3() {
+		return nota3;
+	}
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+	public double getNota4() {
+		return nota4;
+	}
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+	double nota1,nota2,nota3,nota4;
+	
 	public String getNome() {
 		return nome;
 	}
@@ -57,5 +84,17 @@ public class Aluno {
 	}
 	public void setDataMatricula(String dataMatricula) {
 		this.dataMatricula = dataMatricula;
+	}
+	
+	public double getMediaNota() {
+		return (nota1 + nota2 + nota3 + nota4) / 4;
+	}
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaNota();
+		if(media >= 70) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 }
--- src/cursojava/execultavio/Main.java
+++ src/cursojava/execultavio/Main.java
@@ -1,13 +1,18 @@
 package cursojava.execultavio;
 
+import javax.swing.JOptionPane;
+
 import cursojava.classes.Aluno;
 
 public class Main {
 
 	public static void main(String[] args) {
+		String nome = JOptionPane.showInputDialog("Qual o nome do Aluno?");
 		// TODO Auto-generated method stub
 		Aluno aluno1 = new Aluno();
-		aluno1.setNome("Cesar");
+		aluno1.setNome(nome);
+;
+		System.out.print(aluno1.getNome());
 	}
 
 }
